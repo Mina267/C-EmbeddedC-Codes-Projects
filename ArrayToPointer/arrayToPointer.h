3ac6c608bfb8aa72ac2b0d8e5aea7bd9a819b952
mina267 <mina267mina267@gmail.com>
2024-11-30 13:24:47 +0200

    aaaa

--- ArrayToPointer/arrayToPointer.h
+++
@@ -1,7 +0,0 @@
-#ifndef arrayToPointer_H_
-#define arrayToPointer
-int arrayToPointer_LinearSearchchar(char **arr, int size, char ch, int *line);
-void arrayToPointer_BubbleSortingstr(char **arr, int size);
-int str_cmpLarger(char *str1, char* str2);
-int arrayToPointer_LinearSearch(char **arr, int size, char* str);
-#endif // arrayToPointer_H_
